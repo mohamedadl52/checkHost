c42b06b1a3cf4adbcded6ee7dac2a552255b0292
mohamedadl52 <65096134+mohamedadl52@users.noreply.github.com>
2023-07-11 05:06:12 -0700

    Update testF.js
    edit file js

--- testF.js
+++ testF.js
@@ -9,75 +9,76 @@ app.get("/", async (req, res) => {
   const str = fs.readFileSync("allhosts.txt")
   const testst = str.toString().replaceAll("https://", "");
   const testtt = testst.replaceAll("http://", "");
-  const testttt = testtt.replaceAll("www.", "").replaceAll('*.','').replaceAll(';','').replaceAll('\n' , '')
+  const testttt = testtt.replaceAll("www.", "").replaceAll('"', '').replaceAll('*.','').replaceAll(';','').replaceAll('\n' , '')
   const arr = testttt.split(" ");
   const SetA = new Set(arr);
   const lastArray = [...SetA].filter(x=>{
     return x.includes('.')
   });
   // res.json(lastArray)
-  nuum = 0;
+  nuum = 1;
  res.json(lastArray)
   const teeee = async () => {
-    await lastArray.forEach((element) => {
-      // fs.appendFile(
-      //   "allhosts.txt",
-      //     element + " \n",
-      //   function (err) {
-      //     if (err) {
-      //       console.log(err);
-      //     } })
-      elementN = "http://" + element;
-      fs.readFile("testhost.txt", "utf-8", function (err, file) {
+   await  lastArray.forEach((element) => {
+            fs.readFile("testhost.txt", "utf-8", function (err, file) {
         if (err) console.log(err);
         if (file.includes(element)) {
           // console.log("not new")
-          //  console.log('http://'+element)
+           console.log('http://'+element + 'not new')
         } else {
-          fs.appendFile(
-            "testhost.txt",
-             element + " \n",
-            function (err) {
-              if (err) {
-                console.log(err);
-              }
-
-              const test = async () => {
-                await fetchh('https://' + element)
-                  .then((ress) => {
-                    console.log(ress.headers)
-                    return ress;
-                  })
-                  .then((rest) =>{
-                    console.log(
-                      "-------",
-                      rest.url + ' '  + 
-                      rest + '  ' + '('+ nuum++ +')'
-
-                      )
-                      fs.appendFile("vaildhost.txt",
-                        "http://" + element + ' \n'
-                        ,
-                        function (err) {
-                          if (err) {
-                            console.log(err);
-                          }
-            
-            
-                        }
-                      );
-                      
-            
-                  }
+          const test = async () => {
+            await fetchh('https://' + element)
+              .then((ress) => {
+                console.log(ress.headers.get('server'))
+                return ress;
+              })
+              .then((rest) =>{
+                console.log(
+                `${nuum} - ${rest.url} - ${rest.headers.get('server')} - gooooooooooooooooooood`
                   )
-                  .catch((err) => {
-                    console.log("https://" + element +  '('+ nuum++ +')'   + " bad");
-                  })
-              };
-               test()
+                  fs.appendFile(
+                   "vaildhost3.txt",
+                   ` ${nuum} - ${rest.url} - ${rest.headers.get('server')} \n`
+                    ,
+                    function (err) {
+                      if (err) {
+                        console.log(err);
+                      }
+        
+        
+                    }
+                  );
+                  fs.appendFile(
+                    "testhost.txt",
+                    `${element} \n`,
+                    function (err) {
+                      if (err) {
+                        console.log(err);
+                      }
+                  
+                    } );
+                    nuum ++
+                  
+        
+              }
+              )
+              .catch((err) => {
+                fs.appendFile(
+                  "testhost.txt",
+                  `${element} \n`,
+                  function (err) {
+                    if (err) {
+                      console.log(err);
+                    }
+                
+                  } );
+                console.log(`${nuum} - ${element} - bad `);
+                nuum ++
 
-          
-            } );
+              })
+          };
+        test()
+      
         }
       });
 
